560f468e1b51f96d8d88bcc168306e230c4ea970
Hoiy <hoiy927@gmail.com>
2017-02-23 01:08:11 +0800

    First draft for scraper

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-import FB from 'fb'
+var promisifyAll = require('es6-promisify-all')
+var FB = promisifyAll(require('fb'))
+//var FB = require('fb')
+var access_token = require('./lib/access-token')
+var token = require('./token.json')
+
+console.log(token)
+
+access_token.get(token)
+.then((accessToken) => {
+    FB.setAccessToken(accessToken)
+    return FB.napiAsync('disney')
+        .then(res => {
+            console.log(res);
+            return res
+        })
+})
+.catch((err) => {
+    console.log(err)
+})
 
 /*
 module.exports.handler = function(event, context, callback) {
