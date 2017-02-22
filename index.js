f78de33fd1a6977a74aad072debcf65020324295
Hoiy <hoiy927@gmail.com>
2017-02-22 23:16:10 +0800

    Setup linter.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hi');
+import FB from 'fb'
+
+/*
+module.exports.handler = function(event, context, callback) {
+    app_token.acquire((err, res) => {
+        if (err) {
+            context.done(err, null);
+            return;
+        }
+
+        FB.setAccessToken(res);
+        FB.napi(`/${event.pageId}/posts?fields=name,shares`, function(err, res) {
+            context.done(err, res);
+        });
+    });
+};
+*/
