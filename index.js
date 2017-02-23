b72806bc9aacf047d0d223cae543ea04e8922235
Hoiy <hoiy927@gmail.com>
2017-02-23 21:33:27 +0800

    Using es6.

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
-var promisifyAll = require('es6-promisify-all')
-var FB = promisifyAll(require('fb'))
-//var FB = require('fb')
-var access_token = require('./lib/access-token')
-var token = require('./token.json')
+import promisifyAll from 'es6-promisify-all'
+import FB from 'fb'
+import { getAccessToken } from './lib/fb-utils'
+import token from './token.json'
+promisifyAll(FB)
 
-console.log(token)
-
-access_token.get(token)
-.then((accessToken) => {
+getAccessToken(token)
+.then(accessToken => {
     FB.setAccessToken(accessToken)
-    return FB.napiAsync('disney')
+    return FB.napiAsync('disney?fields=about,category,likes,engagement,fan_count,description,name,website')
         .then(res => {
-            console.log(res);
+            console.log(res)
             return res
         })
 })
 .catch((err) => {
     console.log(err)
 })
-
-/*
-module.exports.handler = function(event, context, callback) {
-    app_token.acquire((err, res) => {
-        if (err) {
-            context.done(err, null);
-            return;
-        }
-
-        FB.setAccessToken(res);
-        FB.napi(`/${event.pageId}/posts?fields=name,shares`, function(err, res) {
-            context.done(err, res);
-        });
-    });
-};
-*/
