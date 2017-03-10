655b8bf936b1fd81be2d809976306a293adfeaca
Hoiy <hoiy927@gmail.com>
2017-03-10 22:15:43 +0800

    First draft

---
+++ 0_schema.js
@@ -0,0 +1,132 @@
+import promisifyAll from 'es6-promisify-all'
+import FB from 'fb'
+import BigQuery from '@google-cloud/bigquery'
+import config from './config.json'
+import bsg from 'bigquery-schema-generator'
+
+promisifyAll(FB)
+FB.options({version: 'v2.8'})
+const bigquery = BigQuery(config)
+const dataset = bigquery.dataset('facebook')
+//const pages_table = dataset.table('pages')
+//const posts_table = dataset.table('posts')
+/*
+let schema = bsg({ name: 'Auli\'i Cravalho as Moana | Untold',
+  message: 'Lights, camera, splashin\'! Hear how Auli\'i Cravalho made waves as Moana. 🌊',
+  status_type: 'added_video',
+  type: 'video',
+  link: 'https://www.facebook.com/Disney/videos/10154423106105954/',
+  full_picture: 'https://scontent.xx.fbcdn.net/v/t15.0-10/s720x720/17199474_10154423111340954_2308950645685616640_n.jpg?oh=d981f5ec3aaa1288856ebe4e699514ad&oe=593103BD',
+  picture: 'https://scontent.xx.fbcdn.net/v/t15.0-10/s130x130/17199474_10154423111340954_2308950645685616640_n.jpg?oh=64502a62b1c40634ac8a8f96ad7b88d0&oe=596D8E4F',
+  source: 'https://video.xx.fbcdn.net/v/t42.1790-2/17181276_108864669645545_5934261230897201152_n.mp4?efg=eyJybHIiOjM2MywicmxhIjo1MTIsInZlbmNvZGVfdGFnIjoic3ZlX3NkIn0%3D&rl=363&vabr=202&oh=567edaf58326bfd4ea491471607a29e8&oe=58C364B1',
+  created_time: '2017-03-09T15:00:00+0000',
+  updated_time: '2017-03-09T16:06:15+0000',
+  id: '11784025953_10154423106105954',
+  page_id: '11784025953',
+  share: 47,
+  comment: 15,
+  like: 572,
+  reaction: 659,
+  love: null,
+  haha: null,
+  wow: null,
+  sad: null,
+  angry: null })
+  console.log(schema);
+*/
+
+const pages_opt = {
+    schema: [
+        {
+            name: 'name',
+            type: 'STRING',
+            mode: 'NULLABLE'
+        },
+        {
+            name: 'description',
+            type: 'STRING',
+            mode: 'NULLABLE'
+        },
+        {
+            name: 'about',
+            type: 'STRING',
+            mode: 'NULLABLE'
+        },
+        {
+            name: 'fan_count',
+            type: 'INTEGER',
+            mode: 'NULLABLE'
+        },
+        {
+            name: 'category',
+            type: 'STRING',
+            mode: 'NULLABLE'
+        },
+        {
+            name: 'website',
+            type: 'STRING',
+            mode: 'NULLABLE'
+        },
+        {
+            name: 'talking_about_count',
+            type: 'INTEGER',
+            mode: 'NULLABLE'
+        },
+        {
+            name: 'id',
+            type: 'INTEGER',
+            mode: 'NULLABLE'
+        }
+    ]
+}
+
+dataset.table('pages').delete()
+.then(() =>
+dataset.createTable('pages', pages_opt)
+    .then((results) => {
+        const table = results[0]
+        console.log(`Table ${table.id} created.`)
+        return table
+    })
+    .catch(err => {
+        console.log(err)
+    })
+)
+
+const posts_opt = {
+    schema: [ { name: 'name', type: 'STRING', mode: 'NULLABLE' },
+  { name: 'caption', type: 'STRING', mode: 'NULLABLE' },
+  { name: 'message', type: 'STRING', mode: 'NULLABLE' },
+  { name: 'description', type: 'STRING', mode: 'NULLABLE' },
+  { name: 'status_type', type: 'STRING', mode: 'NULLABLE' },
+  { name: 'type', type: 'STRING', mode: 'NULLABLE' },
+  { name: 'link', type: 'STRING', mode: 'NULLABLE' },
+  { name: 'full_picture', type: 'STRING', mode: 'NULLABLE' },
+  { name: 'picture', type: 'STRING', mode: 'NULLABLE' },
+  { name: 'source', type: 'STRING', mode: 'NULLABLE' },
+  { name: 'created_time', type: 'TIMESTAMP', mode: 'NULLABLE' },
+  { name: 'updated_time', type: 'DATETIME', mode: 'NULLABLE' },
+  { name: 'id', type: 'STRING', mode: 'NULLABLE' },
+  { name: 'page_id', type: 'INTEGER', mode: 'NULLABLE' },
+  { name: 'share', type: 'INTEGER', mode: 'NULLABLE' },
+  { name: 'comment', type: 'INTEGER', mode: 'NULLABLE' },
+  { name: 'like', type: 'INTEGER', mode: 'NULLABLE' },
+  { name: 'reaction', type: 'INTEGER', mode: 'NULLABLE' },
+  { name: 'love', type: 'INTEGER', mode: 'NULLABLE' },
+  { name: 'haha', type: 'INTEGER', mode: 'NULLABLE' },
+  { name: 'wow', type: 'INTEGER', mode: 'NULLABLE' },
+  { name: 'sad', type: 'INTEGER', mode: 'NULLABLE' },
+  { name: 'angry', type: 'INTEGER', mode: 'NULLABLE' } ]
+}
+
+dataset.table('posts').delete().then(() =>
+dataset.createTable('posts', posts_opt)
+    .then((results) => {
+        const table = results[0]
+        console.log(`Table ${table.id} created.`)
+        return table
+    })
+    .catch(err => {
+        console.log(err)
+    })
+)
--- 1_pages.js
+++ 1_pages.js
@@ -1,89 +1,62 @@
 import promisifyAll from 'es6-promisify-all'
 import FB from 'fb'
 import { getAccessToken, PromiseThrottleQueue } from './lib/fb-utils'
-import models from './models'
 import token from './token.json'
 import _ from 'lodash'
 import { sprintf } from 'sprintf-js'
+import BigQuery from '@google-cloud/bigquery'
+import config from './config.json'
+import moment from 'moment'
 
 promisifyAll(FB)
 FB.options({version: 'v2.8'})
-
-console.log(process.argv)
+const bigquery = BigQuery(config)
+const dataset = bigquery.dataset('facebook')
+const pages_table = dataset.table('pages')
+const posts_table = dataset.table('posts')
 
 let min_fan_count = process.argv[5] ? process.argv[5] : 200000
 let seed = process.argv[4] ? process.argv[4] : '11784025953'
 let page_pool = new Set([seed])
 
-const reactions = ['HAHA', 'LOVE', 'WOW', 'SAD', 'ANGRY']
-const reaction = reactions.indexOf('LOVE')
-const query = '%s?fields=id,name,description,about,fan_count,category,website,likes.limit(1000){fan_count},engagement,talking_about_count,posts.limit(100){name,shares,comments.limit(0).summary(total_count),likes.limit(0).summary(total_count),reactions.type(%s).limit(0).summary(total_count),caption,message,description,status_type,type,link,full_picture,picture,source,created_time,updated_time}'
+const reactions = ['NONE', 'HAHA', 'LOVE', 'WOW', 'SAD', 'ANGRY']
+const reaction = reactions.indexOf('NONE')
+const query = '%s?fields=id,name,description,about,fan_count,category,website,likes.limit(1000){fan_count},talking_about_count,posts.limit(100){name,shares,comments.limit(0).summary(total_count),likes.limit(0).summary(total_count),reactions.type(%s).limit(0).summary(total_count),caption,message,description,status_type,type,link,full_picture,picture,source,created_time,updated_time}'
 
-const createPromiseUpsertPage = function(response) {
-    const page = Object.assign({}, response)
-    page.facebook_id = page.id
-    delete page.id
-
-    return models.Page.upsert(page)
+const createPromiseUpsertPage = function(page) {
+    return pages_table.insert(page)
         .then(() => {
-            //console.log(sprintf('Upserted Page %s', page.name));
-            return models.Page.findOne({
-                where: {
-                    facebook_id: {
-                        $eq: page.facebook_id
-                    }
-                }
-            })
-            .then(p => {
-                response.page_id = p.id
-                return response
-            })
+            console.log('Inserted page: ', page.name)
         })
+        .catch(err => console.log(err))
 }
 
-const createPromiseUpsertPosts = function(response) {
-    let posts = _.get(response, 'posts.data', [])
-    posts = posts.map((v) => {
-        const p = Object.assign({}, v, {
-            page_id: response.page_id,
-            facebook_id: v.id,
+const createPromiseUpsertPosts = function(posts) {
+    posts = posts.map(v => {
+        const p = Object.assign(v, {
             share: _.get(v, 'shares.count', 0),
             comment: _.get(v, 'comments.summary.total_count', 0),
             like: _.get(v, 'likes.summary.total_count', 0),
+            reaction: reaction == reactions.indexOf('NONE') ? _.get(v, 'reactions.summary.total_count', 0) : null,
             love: reaction == reactions.indexOf('LOVE') ? _.get(v, 'reactions.summary.total_count', 0) : null,
-            //haha: reaction == reactions.indexOf('HAHA') ? _.get(v, 'reactions.summary.total_count', 0) : null,
-            //wow: reaction == reactions.indexOf('WOW') ? _.get(v, 'reactions.summary.total_count', 0) : null,
-            //sad: reaction == reactions.indexOf('SAD') ? _.get(v, 'reactions.summary.total_count', 0) : null,
-            //angry: reaction == reactions.indexOf('ANGRY') ? _.get(v, 'reactions.summary.total_count', 0) : null
+            haha: reaction == reactions.indexOf('HAHA') ? _.get(v, 'reactions.summary.total_count', 0) : null,
+            wow: reaction == reactions.indexOf('WOW') ? _.get(v, 'reactions.summary.total_count', 0) : null,
+            sad: reaction == reactions.indexOf('SAD') ? _.get(v, 'reactions.summary.total_count', 0) : null,
+            angry: reaction == reactions.indexOf('ANGRY') ? _.get(v, 'reactions.summary.total_count', 0) : null,
+            created_time: moment(v.created_time).utc().format('YYYY-MM-DD HH:mm:ss'),
+            updated_time: moment(v.updated_time).utc().format('YYYY-MM-DD HH:mm:ss'),
         })
-        delete p.id
+        delete p.shares
+        delete p.comments
+        delete p.likes
+        delete p.reactions
         return p
     })
-    return models.Post.bulkCreate(posts, {
-        updateOnDuplicate: [
-            'name',
-            'share',
-            'comment',
-            'like',
-            'love',
-            'haha',
-            'wow',
-            'sad',
-            'angry',
-            'caption',
-            'message',
-            'description',
-            'status_type',
-            'type',
-            'link',
-            'full_picture',
-            'picture',
-            'source'
-        ]
+    return posts_table.insert(posts)
+    .then(() => {
+        console.log(`Inserted ${posts.length} posts.`)
     })
-    .then(() => [
-        console.log(sprintf('Upserted %d posts from %s', posts.length, response.name))
-    ])
+    .catch(err => console.log(err.errors))
 }
 
 const createPromiseFBQuery = function(facebook_id) {
@@ -99,20 +72,27 @@ const createPromiseProcessPage = function(facebook_id) {
     return createPromiseFBQuery(facebook_id)
         .then(res => {
             // queue liked page
-            let like_pages = _.get(res, 'likes.data', [])
+            const like_pages = _.get(res, 'likes.data', [])
+            delete res.likes
+
             for( let page of like_pages ) {
                 if(page.fan_count >= min_fan_count && !page_pool.has(page.id)) {
                     page_pool.add(page.id)
                     queue.push(page.id)
                 }
             }
-            return res
+
+            const posts = _.get(res, 'posts.data', [])
+            delete res.posts
+
+            return [res, posts]
         })
-        .then(res => {
-            return createPromiseUpsertPage(res)
-        })
-        .then(res => {
-            return createPromiseUpsertPosts(res)
+        .then(([page, posts]) => {
+            createPromiseUpsertPage(page)
+            for(let post of posts) {
+                post.page_id = page.id
+            }
+            createPromiseUpsertPosts(posts)
         })
         .catch(err => {
             console.log(err)
